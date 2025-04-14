6a692f75d2b64bf5f3a550d64cb2fbdf1a4a97c8
TristinHuy <zubeo6791@gmail.com>
2025-04-14 18:15:42 +0700

    init project

---
+++ HanoiTowerWinForm/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace HanoiTowerWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SoDia = new NumericUpDown();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            button4 = new Button();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)SoDia).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveBorder;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(671, 429);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.WhiteSmoke;
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Location = new Point(188, 447);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(144, 101);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Phương pháp giải";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 35);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(77, 24);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Đệ quy";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 75);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(65, 24);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Stack";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.WhiteSmoke;
+            button1.Location = new Point(188, 555);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Bắt đầu";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.WhiteSmoke;
+            button2.Location = new Point(362, 555);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Tạm dừng";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.WhiteSmoke;
+            button3.Location = new Point(531, 555);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 6;
+            button3.Text = "Tiếp tục";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // SoDia
+            // 
+            SoDia.BackColor = Color.WhiteSmoke;
+            SoDia.Location = new Point(531, 465);
+            SoDia.Name = "SoDia";
+            SoDia.Size = new Size(150, 27);
+            SoDia.TabIndex = 7;
+            SoDia.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.WhiteSmoke;
+            label1.Location = new Point(531, 510);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Thoi gian hoan thanh:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.hackertypashit2;
+            pictureBox2.Location = new Point(751, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(237, 237);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 9;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.Location = new Point(751, 277);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(237, 215);
+            pictureBox3.TabIndex = 10;
+            pictureBox3.TabStop = false;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(188, 590);
+            button4.Name = "button4";
+            button4.Size = new Size(107, 29);
+            button4.TabIndex = 11;
+            button4.Text = "Test không UI";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.WhiteSmoke;
+            label2.Location = new Point(285, 591);
+            label2.Name = "label2";
+            label2.Size = new Size(137, 27);
+            label2.TabIndex = 12;
+            label2.Text = "Chỉ chạy thuật toán";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1065, 627);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(SoDia);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)SoDia).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private GroupBox groupBox1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private NumericUpDown SoDia;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button4;
+        private Label label2;
+    }
+}
---
+++ HanoiTowerWinForm/Form1.cs
@@ -0,0 +1,483 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace HanoiTowerWinForm
+{
+    public partial class Form1 : Form
+    {
+        // Tower state: 'A', 'B', 'C' -> Stacks of disks
+        private Dictionary<char, Stack<int>> towers;
+        // Total number of disks
+        private int totalDisks;
+        // Default solving method: true for Recursive, false for Iterative
+        private bool useRecursiveMethod = true;
+        public class Timing
+        {
+            private Stopwatch stopwatch;
+
+            public Timing()
+            {
+                stopwatch = new Stopwatch();
+            }
+
+            public void StartTime()
+            {
+                stopwatch.Restart();
+            }
+
+            public void StopTime()
+            {
+                stopwatch.Stop();
+            }
+
+            public TimeSpan Result()
+            {
+                return stopwatch.Elapsed;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region UI Logic: Drawing Towers and Disks
+
+        // Paint event: draw rods and disks
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.DimGray);
+
+            // Rod properties
+            int rodWidth = 10;
+            int rodHeight = 350;
+            int baseY = pictureBox1.Height - 50;
+
+            // Rod positions (match logical names A, B, C)
+            var rodPositions = new Dictionary<char, int>
+            {
+                { 'A', 50 },  // Rod A position
+                { 'B', 250 }, // Rod B position
+                { 'C', 450 }  // Rod C position
+            };
+
+            // Draw rods
+            foreach (var rod in rodPositions.Values)
+            {
+                g.FillRectangle(Brushes.WhiteSmoke, rod - rodWidth / 2, baseY - rodHeight, rodWidth, rodHeight);
+            }
+
+            // Draw disks
+            if (towers != null)
+            {
+                int diskHeight = 20;
+                int maxDiskWidth = 100;
+                int minDiskWidth = 20;
+                int widthStep = totalDisks > 1 ? (maxDiskWidth - minDiskWidth) / (totalDisks - 1) : 0;
+
+                foreach (var rod in rodPositions)
+                {
+                    char rodKey = rod.Key;
+                    int rodX = rod.Value;
+
+                    if (towers[rodKey].Count == 0) continue;
+
+                    // Reverse stack to draw bottom to top
+                    int[] disks = towers[rodKey].ToArray();
+                    Array.Reverse(disks);
+
+                    int y = baseY - diskHeight;
+                    foreach (int disk in disks)
+                    {
+                        int diskWidth = minDiskWidth + (disk - 1) * widthStep;
+                        int x = rodX - diskWidth / 2;
+                        g.FillRectangle(Brushes.Black, x, y, diskWidth, diskHeight);
+
+                        // Draw disk number
+                        string diskNumber = disk.ToString();
+                        SizeF textSize = g.MeasureString(diskNumber, this.Font);
+                        float textX = x + diskWidth / 2 - textSize.Width / 2;
+                        float textY = y + diskHeight / 2 - textSize.Height / 2;
+                        g.DrawString(diskNumber, this.Font, Brushes.White, textX, textY);
+
+                        y -= diskHeight;
+                    }
+                }
+            }
+        }
+
+        // Refresh PictureBox to redraw towers
+        private void PrintTowers()
+        {
+            pictureBox1.Invoke(new Action(() =>
+            {
+                pictureBox1.Refresh();
+            }));
+            Thread.Sleep(10); // Delay for visualization
+        }
+
+        #endregion
+
+        #region Tower of Hanoi Algorithms
+
+        // Recursive Tower of Hanoi solution
+        private void SolveHanoiRecursive(int n, char from, char to, char aux)
+        {
+            if (n == 1)
+            {
+                MoveDisk(from, to);
+                return;
+            }
+
+            SolveHanoiRecursive(n - 1, from, aux, to);
+            MoveDisk(from, to);
+            SolveHanoiRecursive(n - 1, aux, to, from);
+        }
+
+        // Iterative Tower of Hanoi solution
+        private void SolveHanoiIterative(int n, char from, char to, char aux)
+        {
+            var stack = new Stack<Tuple<int, char, char, char>>();
+            stack.Push(Tuple.Create(n, from, to, aux));
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                int disk = current.Item1;
+                char src = current.Item2;
+                char dest = current.Item3;
+                char helper = current.Item4;
+
+                if (disk == 1)
+                {
+                    MoveDisk(src, dest);
+                }
+                else
+                {
+                    stack.Push(Tuple.Create(disk - 1, helper, dest, src));
+                    stack.Push(Tuple.Create(1, src, dest, helper));
+                    stack.Push(Tuple.Create(disk - 1, src, helper, dest));
+                }
+            }
+        }
+
+        // Move a disk from one rod to another
+        private void MoveDisk(char from, char to)
+        {
+            // Pause if needed
+            pauseEvent.Wait();
+
+            // Cancel if requested
+            if (cancellationTokenSource.Token.IsCancellationRequested)
+                return;
+            int disk = towers[from].Pop();
+            towers[to].Push(disk);
+            PrintTowers();
+        }
+
+        //Muốn đo thuần thuật toán thì phải viết lại nhưng vẫn giữ nguyên logic
+        // Code thuật toán cũ quan hệ mật thiết với UI, code mới chạy không in UI
+        private void SolveHanoiRecursiveWithoutUI(int n, char from, char to, char aux)
+        {
+            var localTowers = new Dictionary<char, Stack<int>>
+        {
+        { 'A', new Stack<int>() },
+        { 'B', new Stack<int>() },
+        { 'C', new Stack<int>() }
+        };
+
+            for (int i = n; i >= 1; i--)
+                localTowers['A'].Push(i);
+
+            void Move(char f, char t)
+            {
+                int d = localTowers[f].Pop();
+                localTowers[t].Push(d);
+            }
+
+            void RecursiveSolve(int m, char f, char t, char a)
+            {
+                if (m == 1)
+                {
+                    Move(f, t);
+                    return;
+                }
+
+                RecursiveSolve(m - 1, f, a, t);
+                Move(f, t);
+                RecursiveSolve(m - 1, a, t, f);
+            }
+
+            RecursiveSolve(n, from, to, aux);
+        }
+        private void SolveHanoiIterativeWithoutUI(int n, char from, char to, char aux)
+        {
+            var localTowers = new Dictionary<char, Stack<int>>
+        {
+        { 'A', new Stack<int>() },
+        { 'B', new Stack<int>() },
+        { 'C', new Stack<int>() }
+        };
+
+            for (int i = n; i >= 1; i--)
+                localTowers['A'].Push(i);
+
+            void Move(char f, char t)
+            {
+                int d = localTowers[f].Pop();
+                localTowers[t].Push(d);
+            }
+
+            var stack = new Stack<Tuple<int, char, char, char>>();
+            stack.Push(Tuple.Create(n, from, to, aux));
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                int disk = current.Item1;
+                char src = current.Item2;
+                char dest = current.Item3;
+                char helper = current.Item4;
+
+                if (disk == 1)
+                {
+                    Move(src, dest);
+                }
+                else
+                {
+                    stack.Push(Tuple.Create(disk - 1, helper, dest, src));
+                    stack.Push(Tuple.Create(1, src, dest, helper));
+                    stack.Push(Tuple.Create(disk - 1, src, helper, dest));
+                }
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        // NumericUpDown value changed
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            cancellationTokenSource?.Cancel(); // Cancel ongoing process
+            pauseEvent.Set(); // Ensure it's not stuck in pause
+            InitializeTowers((int)SoDia.Value);
+        }
+
+        // Initialize towers with disks on rod A
+        private void InitializeTowers(int numberOfDisks)
+        {
+            totalDisks = numberOfDisks;
+
+            towers = new Dictionary<char, Stack<int>>
+            {
+                { 'A', new Stack<int>() },
+                { 'B', new Stack<int>() },
+                { 'C', new Stack<int>() }
+            };
+
+            for (int i = numberOfDisks; i >= 1; i--)
+            {
+                towers['A'].Push(i);
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        // Recursive method radio button checked
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                useRecursiveMethod = true;
+        }
+
+        // Iterative method radio button checked
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+                useRecursiveMethod = false;
+        }
+
+        // Start button clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numberOfDisks = (int)SoDia.Value;
+
+            if (numberOfDisks <= 0)
+            {
+                MessageBox.Show("Số đĩa không hợp lệ!");
+                return;
+            }
+
+            // Proper initialization of cancellationTokenSource
+            cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    timing.StartTime(); // Start timing
+
+                    if (useRecursiveMethod)
+                        SolveHanoiRecursive(numberOfDisks, 'A', 'C', 'B');
+                    else
+                        SolveHanoiIterative(numberOfDisks, 'A', 'C', 'B');
+
+                    timing.StopTime(); // Stop timing
+
+                    // Update the UI with the execution time
+                    label1.Invoke(new Action(() =>
+                    {
+                        label1.Text = $"Thời gian hoàn thành: {timing.Result().TotalMilliseconds} ms";
+                    }));
+                }
+                catch (OperationCanceledException)
+                {
+                    // Handle the task being canceled
+                }
+            }, cancellationTokenSource.Token);
+
+        }
+        private void SolveHanoiWithoutUI(int n, char from, char to, char aux)
+        {
+            // Không dùng stack gốc của giao diện
+            var localTowers = new Dictionary<char, Stack<int>>
+    {
+        { 'A', new Stack<int>() },
+        { 'B', new Stack<int>() },
+        { 'C', new Stack<int>() }
+    };
+
+            for (int i = n; i >= 1; i--)
+                localTowers['A'].Push(i);
+
+            void Move(char f, char t)
+            {
+                int d = localTowers[f].Pop();
+                localTowers[t].Push(d);
+            }
+
+            void RecursiveSolve(int m, char f, char t, char a)
+            {
+                if (m == 1)
+                {
+                    Move(f, t);
+                    return;
+                }
+
+                RecursiveSolve(m - 1, f, a, t);
+                Move(f, t);
+                RecursiveSolve(m - 1, a, t, f);
+            }
+
+            RecursiveSolve(n, from, to, aux);
+        }
+
+        #endregion
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            pauseEvent.Reset(); // Pause the process
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            pauseEvent.Set(); // Resume the process
+        }
+
+
+
+        private Timing timing = new Timing(); // Khai báo đối tượng Timing toàn cục
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int numberOfDisks = (int)SoDia.Value;
+
+            if (numberOfDisks <= 0)
+            {
+                MessageBox.Show("Please enter a valid number of disks!");
+                return;
+            }
+
+            cancellationTokenSource = new CancellationTokenSource(); // Reset token hủy bỏ
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    timing.StartTime(); // Bắt đầu đo thời gian
+
+                    if (useRecursiveMethod)
+                        SolveHanoiRecursive(numberOfDisks, 'A', 'C', 'B');
+                    else
+                        SolveHanoiIterative(numberOfDisks, 'A', 'C', 'B');
+
+                    timing.StopTime(); // Kết thúc đo thời gian
+
+                    // Cập nhật UI (vì đang chạy trên thread khác, nên cần dùng Invoke)
+                    label1.Invoke(new Action(() =>
+                    {
+                        label1.Text = $"Thời gian thực thi: {timing.Result().TotalMilliseconds} ms";
+                    }));
+                }
+                catch (OperationCanceledException)
+                {
+                    // Xử lý khi thuật toán bị hủy
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+
+        private CancellationTokenSource cancellationTokenSource;
+        private ManualResetEventSlim pauseEvent = new ManualResetEventSlim(true);
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int numberOfDisks = (int)SoDia.Value;
+
+            if (numberOfDisks <= 0)
+            {
+                MessageBox.Show("Số đĩa không hợp lệ!");
+                return;
+            }
+
+            Task.Run(() =>
+            {
+                var stopwatch = Stopwatch.StartNew();
+
+                if (useRecursiveMethod)
+                    SolveHanoiRecursiveWithoutUI(numberOfDisks, 'A', 'C', 'B');
+                else
+                    SolveHanoiIterativeWithoutUI(numberOfDisks, 'A', 'C', 'B');
+
+                stopwatch.Stop();
+
+                label1.Invoke(new Action(() =>
+                {
+                    label1.Text = $"Thời gian không UI: {stopwatch.ElapsedMilliseconds} ms";
+                }));
+            });
+        }
+    }
+ 
+}
